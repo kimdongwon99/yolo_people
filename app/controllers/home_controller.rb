b6f7667ed294b7ab8f261942e3b7a3dbdbbe99a5
dongwonkay <dongwon@likelion.org>
2017-05-27 05:14:26 +0000

    test

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,11 +1,9 @@
 class HomeController < ApplicationController
   def index
-<<<<<<< HEAD
-    
+
     
     
     @yolo_play = ["travel", "sport", "study"].sample
-=======
->>>>>>> b4dbe4c4885eb3f1e62ffafbb3946796f61a5c80
+
   end
 end
